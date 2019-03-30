b797f87870c11111d2d387dd9724e14c2f0fdcbc
Harmanjot Singh <727172@student.tor.loc>
2019-03-29 21:45:09 -0400

    Harman

---
+++ 727172/727172/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _727172
+{
+    class Program
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("download");
+                Download();
+                Console.ReadLine();
+
+
+
+            }
+            static async void Download()
+            {
+                await Network.download();
+                Console.WriteLine("download complete");
+            }
+
+
+            class Network
+            {
+                static public Task download()
+                {
+                    return Task.Run(
+                    () => ThreadStaticAttribute.Sleep(60000));
+                }
+            }
+        }
+    }
+}
