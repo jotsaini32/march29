744d181373e3ed6794752d39641a72a397d3a5dd
Harmanjot Singh <727172@student.tor.loc>
2019-03-29 21:51:43 -0400

    Final Commit

--- 727172/727172/Program.cs
+++ 727172/727172/Program.cs
@@ -9,33 +9,61 @@ namespace _727172
     class Program
     {
         static void Main(string[] args)
+
         {
+
             Console.WriteLine(" Downloading a file");
+
             Download();
+
             Console.ReadLine();
+
         }
+
+
+
 
         static async void Download()
         {
+
             await Network.Download();
+
             Console.WriteLine("Download Complete");
         }
 
     }
 
+
+
+
+
     class Network
     {
+
+
+
         static ArrayList WbPageContents = new ArrayList();
         public static async Task Download()
         {
 
+
+
+
             HttpClient client = new HttpClient();
+
             var data = await client.GetStringAsync("http://ibm.com");
+
             Console.WriteLine(data);
+
             foreach (var i in data)
             {
+
                 WbPageContents.Add(i);
+
             }
+
         }
+
     }
+
 }
